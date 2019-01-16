f5391ed3f468497151e26b95504f170b0c1a52eb
1579858856@qq.com <1994114abc>
2019-01-16 17:32:49 +0800

    kongzhen

---
+++ src/com/service/PostServiceImpl.java
@@ -0,0 +1,36 @@
+package com.service;
+
+import com.bean.Post;
+import com.dao.PostDao;
+import com.dao.PostDaoImpl;
+
+import java.util.List;
+
+public class PostServiceImpl implements PostService {
+
+    PostDao pd=new PostDaoImpl();
+    @Override
+    public void addPost(Post post) {
+        pd.addPost(post);
+    }
+
+    @Override
+    public List<Post> getAllPost() {
+        return pd.getAllPost();
+    }
+
+    @Override
+    public Post getPostByPid(int pid) {
+        return pd.getPostByPid(pid);
+    }
+
+    @Override
+    public void updatePost(Post post) {
+        pd.updatePost(post);
+    }
+
+    @Override
+    public void delPost(int pid) {
+            pd.delPost(pid);
+    }
+}
