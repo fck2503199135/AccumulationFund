2544fee1dfa469bb38c61f40cb3f4b8c91e6be14
1579858856@qq.com <1994114abc>
2019-01-16 16:44:14 +0800

    kongzhen

--- src/com/dao/DeptDaoImpl.java
+++ src/com/dao/DeptDaoImpl.java
@@ -3,6 +3,7 @@ package com.dao;
 import com.bean.Dept;
 import com.utils.DB;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 
 import java.sql.Connection;
@@ -11,16 +12,58 @@ import java.util.List;
 
 public class DeptDaoImpl implements DeptDao {
     QueryRunner qr=new QueryRunner();
+
     @Override
-    public void addDept(Dept dept) {
+    public void delDept(int did) {
+        Connection con = DB.getcon();
+        try{
+            qr.execute(con,"delete from Dept where did=?",did);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            DB.close();
+        }
+
+    }
+
+    @Override
+    public Dept getDeptByDid(int did) {
+        Connection con = DB.getcon();
+
+        try {
+            return   qr.query(con, "select * from dept where did=?", new BeanHandler<>(Dept.class),did);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.close();
+        }
+
+        return null;
+    }
+
+    @Override
+    public void updateDept(Dept dept) {
         Connection con = DB.getcon();
         try {
-            qr.execute(con,"insert into Dept values(0,?)",dept.getDname());
+            qr.execute(con,"update Dept set dname=? where did=?",dept.getDname(),dept.getDid());
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             DB.close();
         }
+
+    }
+
+    @Override
+    public void addDept(Dept dept) {
+                        Connection con = DB.getcon();
+                        try {
+                            qr.execute(con,"insert into Dept values(0,?)",dept.getDname());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        } finally {
+                            DB.close();
+                        }
 
     }
 
