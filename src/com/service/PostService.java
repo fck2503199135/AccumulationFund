5af0891a947549adf1f0cc2d2ea7b832977a760c
1579858856@qq.com <1994114abc>
2019-01-16 17:32:34 +0800

    kongzhen

--- src/com/service/PostService.java
+++ src/com/service/PostService.java
@@ -1,4 +1,33 @@
 package com.service;
 
+import com.bean.Post;
+
+import java.util.List;
+
 public interface PostService {
+
+
+    /**
+     * 添加
+     */
+    public void addPost(Post post);
+
+    /**
+     * 查询全部
+     */
+    public List<Post> getAllPost();
+    /**
+     * 根据did查找dept
+     */
+    public Post getPostByPid(int pid);
+
+    /**
+     * 删除
+     */
+    public void delPost(int did);
+
+    /**
+     * 修改更新
+     */
+    public void updatePost(Post post);
 }
