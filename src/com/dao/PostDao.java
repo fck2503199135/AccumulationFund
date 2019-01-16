6c3a62d93d293f0ba827431b797b3400dcf695a4
1579858856@qq.com <1994114abc>
2019-01-16 17:31:43 +0800

    kongzhen

--- src/com/dao/PostDao.java
+++ src/com/dao/PostDao.java
@@ -1,4 +1,31 @@
 package com.dao;
 
+import com.bean.Dept;
+import com.bean.Post;
+
+import java.util.List;
+
 public interface PostDao {
+    /**
+     * 添加
+     */
+    public void addPost(Post post);
+
+    /**
+     * 查询全部
+     */
+    public List<Post> getAllPost();
+    /**
+     * 根据did查找dept
+     */
+    public Post getPostByPid(int pid);
+
+    /**
+     * 删除
+     */
+    public void delPost(int did);
+    /**
+     * 修改更新
+     */
+    public void updatePost(Post post);
 }
