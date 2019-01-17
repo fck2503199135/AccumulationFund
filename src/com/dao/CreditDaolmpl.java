389e4b9223e827c0fc994989890404625e19b3b0
fck2503199135 <2503199135@qq.com>
2019-01-17 13:28:14 +0800

    fck

--- src/com/dao/CreditDaolmpl.java
+++ src/com/dao/CreditDaolmpl.java
@@ -3,6 +3,7 @@ package com.dao;
 import com.bean.Credit;
 import com.utils.DB;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 
 import java.sql.Connection;
@@ -16,7 +17,7 @@ public class CreditDaolmpl implements CreditDao {
     public void addCredit(Credit credit) {
         Connection con = DB.getcon();
         try {
-            qr.execute(con,"insert into credit values(null,?,?,?,?,?)",credit.getCname(),credit.getCnum(),credit.getReason(),credit.getStyle(),credit.getImg());
+            qr.execute(con,"insert into credit values(null,?,?,?,?,?,?,?,?,?)",credit.getCdate(),credit.getIndex(),credit.getCname(),credit.getCnum(),credit.getReason(),credit.getStyle(),credit.getImg(),credit.getDeld(),credit.getGoby());
         } catch (SQLException e) {
             e.printStackTrace();
         }finally {
@@ -30,13 +31,106 @@ public class CreditDaolmpl implements CreditDao {
     public List<Credit> getAllCredit() {
         Connection con = DB.getcon();
         try {
-            return qr.query(con,"select * from credit",new BeanListHandler<>(Credit.class));
+            return qr.query(con,"select * from credit where deld is null and goby is null",new BeanListHandler<>(Credit.class));
         } catch (SQLException e) {
             e.printStackTrace();
         }finally {
             DB.close();
         }
 
+        return null;
+    }
+
+    @Override
+    public void updatedeld(Credit credit) {
+        Connection con = DB.getcon();
+        try {
+            qr.execute(con,"update credit set deld = ? where cid = ?",credit.getDeld(),credit.getCid());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DB.close();
+        }
+
+    }
+
+    @Override
+    public void goupdate(Credit credit) {
+        Connection con = DB.getcon();
+        try {
+            qr.execute(con,"update credit set goby = ? where cid = ?",credit.getGoby(),credit.getCid());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DB.close();
+        }
+
+    }
+
+
+    @Override
+    public List<Credit> allDel() {
+        Connection con = DB.getcon();
+        try {
+            return qr.query(con,"select * from credit where deld = 1",new BeanListHandler<>(Credit.class));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DB.close();
+        }
+        return null;
+    }
+
+    @Override
+    public List<Credit> allGo() {
+        Connection con = DB.getcon();
+        try {
+            return qr.query(con,"select * from credit where goby = 0",new BeanListHandler<>(Credit.class));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DB.close();
+        }
+        return null;
+    }
+
+    @Override
+    public int del(int cid) {
+        Connection con = DB.getcon();
+        try {
+            qr.execute(con,"delete from credit where cid = ?",cid);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DB.close();
+        }
+        return 0;
+    }
+
+    @Override
+    public Credit getImg(int cid) {
+        Connection con = DB.getcon();
+        try {
+            return qr.query(con,"select img from credit where cid = ?",new BeanHandler<>(Credit.class),cid);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DB.close();
+        }
+
+        return null;
+    }
+
+    @Override
+    public Credit getbyId(int cid) {
+        Connection con = DB.getcon();
+        try {
+            return qr.query(con,"select * from credit where cid = ?",new BeanHandler<>(Credit.class),cid);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DB.close();
+        }
         return null;
     }
 }
