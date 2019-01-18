9b8e804062186f7503ba90b977cc82bc640dfe2f
1579858856@qq.com <1994114abc>
2019-01-18 11:28:00 +0800

    kongzhen

--- src/com/bean/Post.java
+++ src/com/bean/Post.java
@@ -10,22 +10,52 @@ public class Post {
 
     private  int dname;
 
-
-    public Post() {
-    }
-
-    public Post(int pid, String pname) {
-        this.pid = pid;
-        this.pname = pname;
-    }
-
     @Override
     public String toString() {
         return "Post{" +
                 "pid=" + pid +
                 ", pname='" + pname + '\'' +
+                ", did=" + did +
+                ", dname=" + dname +
                 '}';
     }
+
+    public Post(int pid, String pname, int did, int dname) {
+        this.pid = pid;
+        this.pname = pname;
+        this.did = did;
+        this.dname = dname;
+    }
+
+    public int getDid() {
+        return did;
+    }
+
+    public void setDid(int did) {
+        this.did = did;
+    }
+
+    public int getDname() {
+        return dname;
+    }
+
+    public void setDname(int dname) {
+        this.dname = dname;
+    }
+
+    public Post(int pid, String pname, int did) {
+        this.pid = pid;
+        this.pname = pname;
+        this.did = did;
+    }
+
+    public Post() {
+    }
+
+//    public Post(int pid, String pname) {
+//        this.pid = pid;
+//        this.pname = pname;
+//    }
 
     public int getPid() {
         return pid;
