972b6abe496d75107546be2fb30a8f0cd23393b0
fck2503199135 <2503199135@qq.com>
2019-01-21 14:15:45 +0800

    fck

--- src/com/utils/NowTime.java
+++ src/com/utils/NowTime.java
@@ -5,12 +5,13 @@ import java.util.Date;
 
 public class NowTime {
 
-    public static void ATime(){
+    public static String ATime(){
 
         Date date = new Date();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         String cdate = simpleDateFormat.format(date);
 
+        return cdate;
     }
 
 
