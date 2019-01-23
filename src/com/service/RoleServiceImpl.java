5ae790dd3d0c74e7c61d420b10e813291003fc45
chenshenglai <734629123@qq.com>
2019-01-23 14:14:20 +0800

    chen

--- src/com/service/RoleServiceImpl.java
+++ src/com/service/RoleServiceImpl.java
@@ -11,14 +11,37 @@ import java.sql.Connection;
 import java.util.List;
 
 public class RoleServiceImpl implements RoleService {
+
     RoleDao rd=new RoleDaoImpl();
     @Override
     public List<role> getAllrole() {
         return rd.getAllrole();
     }
 
+    @Override
+    public role getone(int id) {
+        return rd.getone(id);
+    }
+
+    @Override
+    public void update(role r1) {
+      rd.update(r1);
+    }
+
+    @Override
+    public void del(int id) {
+        rd.del(id);
+    }
+
+    @Override
+    public List<role> getone(String rid) {
+        return rd.getone(rid);
+    }
+
+
     public static void main(String[] args) {
-//        RoleDao rd=new RoleDaoImpl();
-//        System.out.println(rd.getAllrole());
+       RoleService rs=new RoleServiceImpl();
+              System.out.println(rs.getone("R003"));
+
     }
 }
