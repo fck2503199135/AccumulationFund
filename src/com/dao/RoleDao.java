78f504fe16decaa5eca6655e4bf25b3ad4f9d510
chenshenglai <734629123@qq.com>
2019-01-23 15:02:29 +0800

    chen

--- src/com/dao/RoleDao.java
+++ src/com/dao/RoleDao.java
@@ -7,4 +7,8 @@ import java.util.List;
 
 public interface RoleDao  {
     List<role> getAllrole();
+    role getone(int id);
+    void  update(role r1);
+    void del(int id);
+    List<role> getone(String rid);
 }
